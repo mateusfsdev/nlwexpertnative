8c985ef9eb2cf117453deead923cd2ca2f3a5c83
mateus sant'ana <mateusfdev@outlook.com>
2024-02-07 13:32:42 -0300

    the first bro 💥💥💥

---
+++ src/app/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text  } from 'react-native'
+
+export default function Home(){
+return (
+  <View className=' flex-1 items-center justify-center'>
+    <Text className='text-green-400 font-bold text-4xl '>your order?</Text>
+  </View>
+)
+
+}
