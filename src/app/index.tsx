1ae6661ef456382eb5778d0374080e22c895de73
mateus sant'ana <mateusfdev@outlook.com>
2024-02-08 10:16:01 -0300

    useState categories 🦽🥤

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,10 +2,20 @@ import { View, FlatList } from 'react-native'
 import { CategoryButton } from '../components/categoryButton'
 import { CATEGORIES } from '../utils/data/products'
 import { Header } from '../components/header'
+import { useState } from 'react'
+
 
 
 
 export default function Home(){
+  const [category, setCategory] = useState(CATEGORIES[0])
+
+  function handleCategorySelect(selectedCategory: string){
+    setCategory(selectedCategory)
+
+  }
+
+
 return (
   <View className=' flex-1 mt-5'>
    <Header title='Faça seu pedido'cartQuantityItems={5}/>
@@ -13,7 +23,10 @@ return (
    <FlatList 
     data={CATEGORIES}
     keyExtractor={(item) => item}
-    renderItem={({ item}) => <CategoryButton title={item}/>}
+    renderItem={({ item}) =>
+      <CategoryButton
+      isSelected={item === category}
+      title={item} onPress={() => handleCategorySelect(item)}/>}
     horizontal
     className='max-h-10 mt-5'
     showsHorizontalScrollIndicator={false}
