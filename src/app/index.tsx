233ef2ee52b88f132cdfb876ff27f2a5ec02547d
mateus sant'ana <mateusfdev@outlook.com>
2024-02-08 09:56:20 -0300

    clsx install create categoryButton 🧁🧁

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,14 +1,16 @@
 import { View } from 'react-native'
 import { Header } from '../components/header'
+import { CategoryButton } from '../components/categoryButton'
 
 
 export default function Home(){
 return (
   <View className=' flex-1 mt-5'>
-   <Header
-   title='Faça seu pedido'
-   cartQuantityItems={5}
-   />
+   <Header title='Faça seu pedido'cartQuantityItems={5}/>
+  <View className='flex-row justify-evenly'>
+    <CategoryButton title='lanche do dia' isSelected></CategoryButton>
+    <CategoryButton title='Combos'></CategoryButton>
+  </View>
 
   </View>
 )
