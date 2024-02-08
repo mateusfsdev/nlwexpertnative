7177eb522cb4abec0f38c0e5b03a203095515601
mateus sant'ana <mateusfdev@outlook.com>
2024-02-08 18:49:23 -0300

    delete items to cart 🎃🎃🎃

--- src/stores/cartStore.ts
+++ src/stores/cartStore.ts
@@ -1,6 +1,8 @@
 import { create } from 'zustand'
 import { ProductProps } from '../utils/data/products'
+import { createJSONStorage, persist } from 'zustand/middleware'
 import * as cartInMemory from '@/src/stores/helpers/cartInMemory'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 export type ProductCartProps = ProductProps & {
   quantity: number
@@ -12,7 +14,8 @@ type StateProps = {
   remove: (productId: string) => void
 }
 
-export const useCartStore = create<StateProps>((set) => ({
+export const useCartStore = create(
+  persist<StateProps>((set) => ({
   products: [],
   add: (product: ProductProps) => set((state) => ({
     products: cartInMemory.add(state.products, product),
@@ -21,4 +24,9 @@ export const useCartStore = create<StateProps>((set) => ({
   set((state) =>({
     products:  cartInMemory.remove(state.products, productId)
   }))
-}))
+}), {
+  name:'nlw-expert:cart',
+  storage: createJSONStorage(() => AsyncStorage)
+
+}
+))
