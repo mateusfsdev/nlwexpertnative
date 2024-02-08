3b6282b5b8739012e2d9bd87fc18a771ba22527c
mateus sant'ana <mateusfdev@outlook.com>
2024-02-08 10:07:24 -0300

    style edit categories 🎃✨

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,16 +1,24 @@
-import { View } from 'react-native'
-import { Header } from '../components/header'
+import { View, FlatList } from 'react-native'
 import { CategoryButton } from '../components/categoryButton'
+import { CATEGORIES } from '../utils/data/products'
+import { Header } from '../components/header'
+
 
 
 export default function Home(){
 return (
   <View className=' flex-1 mt-5'>
    <Header title='Faça seu pedido'cartQuantityItems={5}/>
-  <View className='flex-row justify-evenly'>
-    <CategoryButton title='lanche do dia' isSelected></CategoryButton>
-    <CategoryButton title='Combos'></CategoryButton>
-  </View>
+
+   <FlatList 
+    data={CATEGORIES}
+    keyExtractor={(item) => item}
+    renderItem={({ item}) => <CategoryButton title={item}/>}
+    horizontal
+    className='max-h-10 mt-5'
+    showsHorizontalScrollIndicator={false}
+    contentContainerStyle={{ gap: 12, paddingHorizontal:20 }}
+  />
 
   </View>
 )
