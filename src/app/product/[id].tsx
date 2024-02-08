8e445e563e25e8f1248dd6df7c1d5aeea1eca290
mateus sant'ana <mateusfdev@outlook.com>
2024-02-08 16:36:22 -0300

    callback certify for items id

--- src/app/product/[id].tsx
+++ src/app/product/[id].tsx
@@ -16,9 +16,11 @@ export default function Product(){
 
   const product = PRODUCTS.find((item) => item.id === id)
   function handleAddToCart(){
+    if(product){
     cartStore.add(product!)
     navigation.goBack()
   }
+}
 
   if(!product) {
     return <Redirect href='/'/>
@@ -33,6 +35,7 @@ export default function Product(){
       />
 
       <View className="p-5 mt-8 flex-1">
+      <Text className="text-zinc-100 text-2xl font-heading">{product.title}</Text>
         <Text className='text-lime-400 text-2xl font-heading my-2'>
           {formatCurrency(product.price)}
         </Text>
